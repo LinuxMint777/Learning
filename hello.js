7d9768815692477b0c86576ac82fb006d278abfc
linuxmint777 <diustudentofcse@gmail.com>
2023-09-17 18:25:22 +0600

    add hello.js included git command here.

--- hello.js
+++ hello.js
@@ -1,1 +1,45 @@
-console.log('Hello world')
+/**
+ * cd ..
+ * mkdir folderName
+ * cd folderName
+ * type nul> app.js
+ * code .
+ * 
+ * git init
+ * git clone http://address/
+ * 
+ * git status
+ * 
+ * git add --all
+ * git add -A
+ * git add .
+ * git add *
+ * git add *.js
+ * 
+ * git reset
+ * git reset --hard
+ * 
+ * git commit -m "message here"
+ * git reset HEAD~
+ * 
+ * git rm fileName.txt
+ * git rm fileName.txt -f
+ * git rm --cashed file.txt
+ * git rm folderName
+ * git rm -r folderName
+ * 
+ * git branch
+ * git branch branchName
+ * git checkout branchName
+ * 
+ * git merge main -m "I a marge on development branch."
+ * git merge development -m "I am merge on main branch."
+ * 
+ * git push origin master
+ * git push origin development
+ * 
+ * git fetch
+ * git merge
+ * git pull
+ * 
+ */
