0429ad88b5f4874ce3afc8883846bc290a2eac46
LinuxMint777 <diustudentofcse@gmail.com>
2023-10-12 17:03:39 +0600

    Create DOM Folder and dom.js file added

---
+++ DOM/dom.js
@@ -0,0 +1,4 @@
+/**
+ * document, document.all, document.title, document.images, document.links, document.location, Document => HTML Document => document, getElementByID(), getElementsByClassName(), querySelector(), querySelectorAll(), getElementByTagName(), children, parentElement, createElement(), append, appendChild, insertBefore(), target, className, setAttribute(), clsssList(), style.color = 'red', for loop use, for of loop use, for in loop use, closeset(), nextElementSibling, prevElementSibling, addEventListener(), click, dblclick, mousernter, mouseleave, mouseover, mouseout, mousedown, mouseup, keyup, keydown, keypress, focus, blur, cut, pase, copy (not sure), input, submit, event, event.preventDefault(), target.value,
+ */
+
