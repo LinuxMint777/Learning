529204e017a4d4c24168784375c85ac8bf257785
linuxmint777 <diustudentofcse@gmail.com>
2023-10-31 23:58:05 +0600

    Add Jahid.js file. This files are Some Hints about our Learning Path

---
+++ Jahid.js
@@ -0,0 +1,55 @@
+/**
+ * React Learning Path.
+ * 
+ * 1. Web Foundamental: Browser, Dev Tools, Terminal, Git & Github - learnwithsumit, rendering - learnwithsumit (search virtual dom).
+ * 
+ * 2. HTML: Hypertext Markup Language - w3schools.com, HTML One Video (Tech Gun HTML Tutorial).
+ * 
+ * 3. CSS: Cascading Style Sheet - w3schools.com, Css One Video (Tech Gun CSS Tutorial), css flexbox & css grid - learnwithsumit. 
+ * 
+ * 4. Tailwindcss: Css Libray (tailwindcss) - learnwithsumit.
+ * 
+ * 5. JavaScript: JavaScript Basic Playlist => DOM Playlist => Modern Javascript Playlist => Think a Javascript => Javascript Tricks & Tips - learnwithsumit, Javascript One Video (Tech Gun JS Tutorial)
+ * 
+ * 6. React: React Playlist - learnwithsumit, React One Video (Tech Gun React Tutorial)
+ * 
+ * 7. Nodejs: Express or NextJs - learwithsumit, ExpressJS or NextJS Video Tutorial Search to Youtube. 
+ */
+
+
+
+
+
+
+
+
+
+
+/**
+ * Web Development Beginner Questions:
+ * 
+ * 1. How does web works: Internet and Web, client GoogleChrome => ISP => DNS return IP Address => ISP => Client GoogleChrome => Orginal ip address hit and response by server.
+ * 
+ * 2. DNS: Domain Name Server (Root Server, TLD Server, Name Server)
+ * 
+ * 3. How does web browser work: User Interface, Broser enginne (Data Presistance), render enginee => Network, JS interpreater, Backend Interface.
+ * All information collected => Parse html to DOM => Render Tree => Layout => Paint.
+ * 
+ * 4. What is Frontend: HTML, CSS (Bootstrap, Tailwindcss), JS (React, Angular),
+ * 
+ * 5. What is Backend: Nodejs (Express, Nextjs), Mongodb (NoSQL Database).
+ * 
+ * 6. Frontend vs Backend vs Fullstack : Fullstack Website Deploy.
+ * 
+ * 7. Domain and Webhosting: Sharehost, Virtual Private Server, Platform as a service, dedicated server.
+ * 
+ * 8. HTTP: Hypertext transfer protocal, (Header: content-type, os information), get, post, put, delete, status code (200-ok, 404-page not found, 500-internal server error, 503-service unavailable)
+ * 
+ * 9. Single Page Application: As like Application. Single app act as a application. Browser not reload. 
+ * 
+ * 10. Progressive Web: Website and Application act. 
+ * 
+ * 11. API: Application Programming interface (REST API), API Only response or return only result (not html, css, js code).
+ * 
+ * 12. Libray vs Framework: You control libray But Framework control you.
+ */
